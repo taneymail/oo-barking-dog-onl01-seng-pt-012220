2dec7a8a7cb824f795fd76d290b8d1c87c4fc1d3
T S <taneymail@gmail.com>
2020-02-06 20:02:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,18 @@
-
+class Dog
+ 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+ 
+  def name
+    @this_dogs_name
+  end
+end
+ 
+fido = Dog.new
+fido.name = "Fido"
+ 
+puts fido.name
 
 
 

cf9ba0dc9d56fc0476c50e4d92bede94fc9f86b1
T S <taneymail@gmail.com>
2020-02-06 19:46:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,26 @@
 class dog
-  def name=(dog_name)
+  
+  def name = (dog_name)
+    @name = dog_name
+  end
+  
+  def name
+    @name
+  end
+end
+
+class dog 
+  def bark
+      puts "woof!"
+  end
+end
+
+fido = Dog.new 
+fido.name = "Fido" 
+
+fido.name
+
+
+
+
 
